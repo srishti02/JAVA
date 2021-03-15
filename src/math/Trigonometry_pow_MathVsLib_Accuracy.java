d2e78732d6c6dad5bdcdbc7826681457e7989c9f
Margaret2702 <nesarajm@uwindsor.ca>
2021-03-15 22:53:05 +0530

    Accuracy decimal

--- src/math/Trigonometry_pow_MathVsLib_Accuracy.java
+++ src/math/Trigonometry_pow_MathVsLib_Accuracy.java
@@ -7,18 +7,17 @@ import org.junit.jupiter.api.Test;
 import math.MyMath.Trigonometry;
 
 class Trigonometry_pow_MathVsLib_Accuracy {
-	
+	double actual=0.0;
 	double expected=0.0;
-double actual=0.0;
 	MyMath mymath=new MyMath();
+	double decimalDigits=0.0001;
 
 	@Test()
 	void accuracyPowpt5() throws FactorialException {
 		actual=MyMath.pow(5, 5);
  	   System.out.println("The value of pow(5,5) is " +actual);
 	    //To validate the value of pow(5,5)
-	 	assertEquals(Math.pow(5, 5),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.pow(5, 5),actual ,decimalDigits);        
            
 	}
 	@Test()
@@ -26,8 +25,7 @@ double actual=0.0;
 		actual=MyMath.pow(10000.9999,150);
  	   System.out.println("The value of pow(10000.9999,150) is " +actual);
 	    //To validate the value of pow(10000.9999,150)
-	 	assertEquals(Math.pow(10000.9999,150),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.pow(10000.9999,150),actual ,decimalDigits);        
            
 	}
 	
@@ -36,8 +34,7 @@ double actual=0.0;
 		actual=MyMath.degreesToRadian(60);
  	   System.out.println("The value of degreesToRadian(60) is " +actual);
 	    //To validate the value of degreesToRadian(60)
-	 	assertEquals(Math.toRadians(60),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.toRadians(60),actual ,decimalDigits);        
            
 	}
 	
@@ -46,8 +43,7 @@ double actual=0.0;
 		actual=MyMath.degreesToRadian(45);
  	   System.out.println("The value of degreesToRadian(45) is " +actual);
 	    //To validate the value of degreesToRadian(45)
-	 	assertEquals(Math.toRadians(45),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.toRadians(45),actual ,decimalDigits);        
            
 	}
 	
@@ -56,8 +52,7 @@ double actual=0.0;
 		actual=MyMath.degreesToRadian(0.8E60);
  	   System.out.println("The value of degreesToRadian(0.8E60) is " +actual);
 	    //To validate the value of degreesToRadian(0.8E60)
-	 	assertEquals(Math.toRadians(0.8E60),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.toRadians(0.8E60),actual ,decimalDigits);        
            
 	}
 	
@@ -66,8 +61,7 @@ double actual=0.0;
 		actual=Trigonometry.sin(1.57079);
  	  // System.out.println("The value of sin(1.57079) is " +actual);
 	    //To validate the value of sin(1.57079)
-	 	assertEquals(Math.sin(1.57079),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.sin(1.57079),actual ,decimalDigits);        
            
 	}
 	
@@ -77,8 +71,7 @@ double actual=0.0;
 		actual=Trigonometry.sin(-1);
  	  // System.out.println("The value of sin(-1) is " +actual);
 	    //To validate the value of sin(-1)
-	 	assertEquals(Math.sin(-1),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.sin(-1),actual ,decimalDigits);        
            
 	}
 	
@@ -87,8 +80,7 @@ double actual=0.0;
 		actual=Trigonometry.cos(1.57079);
  	  // System.out.println("The value of cos(1.57079) is " +actual);
 	    //To validate the value of cos(1.57079)
-	 	assertEquals(Math.cos(1.57079),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.cos(1.57079),actual ,decimalDigits);        
            
 	}
 	
@@ -98,8 +90,7 @@ double actual=0.0;
 		actual=Trigonometry.cos(-1);
  	  // System.out.println("The value of cos(-1) is " +actual);
 	    //To validate the value of cos(-1)
-	 	assertEquals(Math.cos(-1),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.cos(-1),actual ,decimalDigits);        
            
 	}
 	
@@ -108,28 +99,25 @@ double actual=0.0;
 		actual=Trigonometry.tan(1.57079);
  	  // System.out.println("The value of tan(1.57079) is " +actual);
 	    //To validate the value of tan(1.57079)
-	 	assertEquals(Math.tan(1.57079),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.tan(1.57079),actual ,decimalDigits);        
            
 	}
 	
 	@Test()
 	void tanNeg1() throws FactorialException {
 		actual=Trigonometry.tan(-1);
- 	  // System.out.println("The value of cos(-1) is " +actual);
 	    //To validate the value of cos(-1)
-	 	assertEquals(Math.cos(-1),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.tan(-1),actual ,decimalDigits);        
            
 	}
 
 	@Test()
 	void tanDegtoRad90() throws FactorialException {
 		actual=Trigonometry.tan(MyMath.degreesToRadian(90));
- 	  // System.out.println("The value of tan(MyMath.degreesToRadian(90) is " +actual);
+ 	    System.out.println("The value of tan(MyMath.degreesToRadian(90) is " +actual);
+		System.out.println(Math.tan(Math.toRadians(90)));
 	    //To validate the value of tan(MyMath.degreesToRadian(90)
-	 	assertEquals(Math.tan(Math.toRadians(90)),actual , "Expected is :-1\r\n" + 
-	 			" Actual is " +actual);        
+	 	assertEquals(Math.tan(Math.toRadians(90)),actual ,decimalDigits);        
            
 	}
 	}
